c637208010800ea4bc5b11ba7ed35c12b2e3b290
TATARYA22 <142767389+TATARYA22@users.noreply.github.com>
2025-05-28 00:44:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-import pickle
 import numpy as np
 import pickle
 
 
-model = pickle.load(open('model/linear.pkl', 'wb'))  
+# Load model
+model = pickle.load(open('model.pkl', 'rb'))
 
-app = st.text_input(__name__, template_folder='templates')
-
-tensi_mapping = {'Normal': 0, 'Tinggi': 1, 'Rendah': 2}
+# Mapping kategori
+tensi_mapping = {'Normal': 0, 'Pre Hipertensi': 1, 'Hipertensi': 2}
 jantung_mapping = {'Normal': 0, 'Tidak Normal': 1}
-
 
 def preprocess_input(sistolik, diastolik, denyut_nadi, suhu, kesimpulan_tensi, kesimpulan_jantung):
     tensi = tensi_mapping.get(kesimpulan_tensi, -1)
     jantung = jantung_mapping.get(kesimpulan_jantung, -1)
-
     return np.array([[sistolik, diastolik, denyut_nadi, suhu, tensi, jantung]])
 
 @app.route('/')
@@ -23,43 +20,19 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    
     sistolik = float(request.form['sistolik'])
     diastolik = float(request.form['diastolik'])
-    denyut_nadi = float(request.form['denyut_nadi'])
+    denyut_nadi = float(request.form['denyut'])
     suhu = float(request.form['suhu'])
-    kesimpulan_tensi = request.form['kesimpulan_tensi']
-    kesimpulan_jantung = request.form['kesimpulan_jantung']
+    tensi = request.form['tensi']
+    jantung = request.form['jantung']
 
-  
-    input_data = preprocess_input(sistolik, diastolik, denyut_nadi, suhu, kesimpulan_tensi, kesimpulan_jantung)
+    input_data = preprocess_input(sistolik, diastolik, denyut_nadi, suhu, tensi, jantung)
     prediction = model.predict(input_data)
 
-    return render_template('index.html', prediction_text=f'Hasil prediksi kondisi kesehatan: <b>{prediction[0]}</b>')
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    # Ambil input dari form
-    features = [float(x) for x in request.form.values()]
-
-    # Fitur: [Sistolik, Diastolik, Denyut, Suhu, Tensi, Jantung]
-    final_features = np.array(features).reshape(1, -1)
-
-    # Prediksi
-    prediction = model.predict(final_features)
-
-    # Format output
     hasil = 'Sehat' if prediction[0] == 0 else 'Tidak Sehat'
 
     return render_template('index.html', prediction_text=f'Hasil Prediksi Kondisi Kesehatan: <b>{hasil}</b>')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
